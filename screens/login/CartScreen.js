3a1cce1159a3e93c6137c4485fc1c85034b8b0b6
spartacus <spartacus.vipul@gmail.com>
2023-11-26 01:39:49 -0500

    Added method to send data to server in cartscreen.

--- screens/login/CartScreen.js
+++ screens/login/CartScreen.js
@@ -1,40 +1,91 @@
 import React from 'react';
 import { useState } from 'react';
 import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+import { useAuth } from '../../store/AuthContext';
 
 const CartScreen = ({ route }) => {
     const [cartItems, setCartItems] = useState(route.params.items || []);
+    const { loggedInUserName } = useAuth();
 
     const handleRemoveItem = (indexToRemove) => {
         const newItems = cartItems.filter((_, index) => index !== indexToRemove);
         setCartItems(newItems);
     };
 
+    const sendDataToServer = () => {
+        console.log("send data to server");
+        const currentTime = new Date();
+        // Formatting the date and time as a string (e.g., "2023-03-21T10:30:15")
+        const visitTime = currentTime.toISOString();
+        const prescriptionData = {
+            doctor: loggedInUserName,
+            patient: "patient1",
+            disease: "", 
+            visitTime: visitTime, // assuming you have a way to set these
+            simpletiles: cartItems.filter(item => item.flag === 'simple').map(item => item.text),
+            complextiles: cartItems.filter(item => item.flag === 'complex').map(item => ({ type: item.type, todo: item.todo })),
+        };
+
+        console.log("Send data to server:", JSON.stringify(prescriptionData, null, 2));
+        const apiEndpoint = 'https://traficml.uc.r.appspot.com/get-all-sessions';
+        fetch(apiEndpoint, {
+            method: 'POST', // or 'PUT' if you are updating data
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(JSON.stringify(prescriptionData, null, 2)),
+            })
+            .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+            })
+            .then(data => {
+            console.log('Success:', data);
+            // Handle the response data
+            })
+            .catch(error => {
+            console.error('Error:', error);
+            // Handle errors
+            });
+        
+    };
+
     return (
-        <ScrollView>
-            <View style={styles.tileContainer}>
-                <Text style={styles.title}>Cart Items:</Text>
-                {cartItems.map((item, index) => (
-                    <TouchableOpacity 
-                      key={index} 
-                      style={styles.tile} 
-                      onPress={() => handleRemoveItem(index)}
-                    >
-                        <Text style={styles.text}>{item.text}</Text>
-                    </TouchableOpacity>
-                ))}
-            </View>
-        </ScrollView>
+        <View style={styles.container}>
+            <ScrollView style={styles.scrollView}>
+                <View style={styles.tileContainer}>
+                    <Text style={styles.title}>Cart Items:</Text>
+                    {cartItems.map((item, index) => (
+                        <TouchableOpacity 
+                            key={index} 
+                            style={styles.tile} 
+                            onPress={() => handleRemoveItem(index)}
+                        >
+                            <Text style={styles.text}>{item.text}</Text>
+                        </TouchableOpacity>
+                    ))}
+                </View>
+            </ScrollView>
+            <TouchableOpacity 
+                style={styles.button} 
+                onPress={() => sendDataToServer()}
+            >
+                <Text style={styles.buttonText}>Send Prescription</Text>
+            </TouchableOpacity>
+        </View>
     );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 10,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'space-between',
+    },
+    scrollView: {
+        flex: 1,
+    },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
@@ -60,6 +111,14 @@ const styles = StyleSheet.create({
   },
   text: {
     color: 'black', // Black text color for general text
+  },
+  button: {
+    backgroundColor: '#007ACC', // Button background color
+    padding: 15, // Button padding
+    alignItems: 'center', // Center text horizontally
+    justifyContent: 'center', // Center text vertically
+    width: '100%', // Make the button extend full width
+    borderRadius: 5, // Rounded corners
   },
 });
 
--- screens/login/Scan.js
+++ screens/login/Scan.js
@@ -1,6 +1,6 @@
 // QRScannerScreen.js
 import React from 'react';
-import { View, Button } from 'react-native';
+import { View, Button, TouchableOpacity, Text, StyleSheet } from 'react-native';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 
 const Scan = ({ navigation }) => {
@@ -19,9 +19,34 @@ const Scan = ({ navigation }) => {
         onRead={handleScan}
         showMarker
       />
+      <TouchableOpacity 
+        style={styles.button} 
+        onPress={() => navigation.navigate('SessionsPreviousHistory')}
+        activeOpacity={0}
+        >
+        <Text style={styles.text}> </Text>
+       </TouchableOpacity>
       <Button title="Close Scanner" onPress={() => navigation.goBack()} />
     </View>
   );
 };
 
+
+const styles = StyleSheet.create({
+
+  text: {
+    color: 'black', // Black text color for general text
+  },
+  button: {
+      display: 'transparent',
+      backgroundColor: '#007ACC', // Button background color
+      padding: 25, // Button padding
+      alignItems: 'center', // Center text horizontally
+      justifyContent: 'center', // Center text vertically
+      width: '100%', // Make the button extend full width
+      // borderRadius: 5, // Rounded corners
+      opacity: 0, // Make the button invisible but still clickable
+    },
+  // ... other styles ...
+});
 export default Scan;
